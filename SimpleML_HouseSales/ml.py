79e53386e57e276f04e10c7ae40d7498e5734e16
LeeTan <leedtan@gmail.com>
2018-09-20 18:44:26 -0400

    reorganizie visualization code at end

--- SimpleML_HouseSales/ml.py
+++ SimpleML_HouseSales/ml.py
@@ -1,27 +1,44 @@
-def apply_clipped_optimizer(opt_fcn,
-                            loss,
-                            clip_norm=.1,
-                            clip_single=.03,
-                            clip_global_norm=False):
-    gvs = opt_fcn.compute_gradients(loss)
+def plot_by_group(yhat_all, val_Y_all, increment=50, metric='abs'):
 
-    if clip_global_norm:
-        gs, vs = zip(*[(g, v) for g, v in gvs if g is not None])
-        capped_gs, grad_norm_total = tf.clip_by_global_norm([g for g in gs],
-                                                            clip_norm)
-        capped_gvs = list(zip(capped_gs, vs))
+    if metric == 'abs':
+        err = np.abs((yhat_all - val_Y_all) / val_Y_all)
+        title = "Absolute Percent Errors by house sales group"
     else:
-        grad_norm_total = tf.sqrt(
-            tf.reduce_sum([
-                tf.reduce_sum(tf.square(grad)) for grad, var in gvs
-                if grad is not None
-            ]))
-        capped_gvs = [(tf.clip_by_value(grad, -1 * clip_single, clip_single),
-                       var) for grad, var in gvs if grad is not None]
-        capped_gvs = [(tf.clip_by_norm(grad, clip_norm), var)
-                      for grad, var in capped_gvs if grad is not None]
+        err = (yhat_all - val_Y_all) / val_Y_all
+        title = "Raw Percent Errors by house sales group"
 
-    optimizer = opt_fcn.apply_gradients(
-        capped_gvs, global_step=tf.train.get_global_step())
+    true_bin = ((val_Y_all // increment) + 1) * increment
+    err_bins = {}
+    for pe, tb in zip(err, true_bin):
+        if not tb in err_bins.keys():
+            err_bins[tb] = []
+        err_bins[tb] += [pe * 100]
 
-    return optimizer, grad_norm_total
+    err_list = [err_bins[key] for key in sorted(err_bins)]
+    medians = [np.median(err_list[idx]) for idx in range(len(err_list))]
+    q25s = [np.percentile(err_list[idx], 25) for idx in range(len(err_list))]
+    q75s = [np.percentile(err_list[idx], 75) for idx in range(len(err_list))]
+    keys = [key for key in sorted(err_bins)]
+    _, ax = plt.subplots(nrows=1, ncols=1, sharex=True, figsize=(15, 5))
+
+    ax.set(
+        xlabel='true time to sell house',
+        ylabel='IQR of percent error by group',
+        title=title)
+
+    # Add std deviation bars to the previous plot
+    ax.errorbar(
+        keys,
+        medians,
+        yerr=[[medians[i] - q25s[i] for i in range(len(medians))],
+              [q75s[i] - medians[i] for i in range(len(medians))]],
+        fmt='-o')
+    xticks = [
+        str(key - 50)[:-2] + ' to ' + str(key)[:-2] + '\n' + 'size group:' +
+        str(len(err_list[idx])) for idx, key in enumerate(sorted(err_bins))
+    ]
+    ax.set_xlim([keys[0] - increment / 2, keys[-1] + increment / 2])
+    ax.set_xticks(np.arange(np.min(keys), np.max(keys) + 1, increment))
+    ax.set_xticklabels(xticks)
+    plt.show()
+    return err_bins
